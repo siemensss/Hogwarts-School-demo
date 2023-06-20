28c1d9ae0a315fa2a6999288b7208ed4bb659c6f
Semyon <sema.perstnev.01@mail.ru>
2023-06-20 23:32:12 +0300

    school

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,46 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.*;
+
+@Service
+public class FacultyService {
+
+    private final Map<Long, Faculty> faculties = new HashMap<>();
+    private long idCounter = 0;
+
+    public Faculty addFaculty(Faculty faculty) {
+        faculty.setId(idCounter++);
+        faculties.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty findFaculty(long id) {
+        return faculties.get(id);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        if (!faculties.containsKey(faculty.getId())) {
+            return null;
+        }
+        faculties.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty deleteFaculty(long id) {
+        return faculties.remove(id);
+    }
+
+    public Collection<Faculty> findByColor(String color) {
+        ArrayList<Faculty> result = new ArrayList<>();
+        for (Faculty faculty : faculties.values()) {
+            if (Objects.equals(faculty.getColor(), color)) {
+                result.add(faculty);
+            }
+        }
+        return result;
+    }
+}
